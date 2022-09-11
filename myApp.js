8a29e365cdff8e975ef90ee6f08b85b9136ec350
Lukas Meinhart <100217325+Drako222@users.noreply.github.com>
2022-09-11 10:32:33 +0200

    Testing replit

--- myApp.js
+++ myApp.js
@@ -1,4 +1,14 @@
 let express = require('express');
 let app = express();
 
+// app.get(function (req, res) {
+//   if (req === '/') {
+//     res.send('Hello Express');
+//   }
+// });
+
+// console.log(app.get('/', res));
+
 console.log('Hello World');
+
+module.exports = app;
