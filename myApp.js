c352413e0067cd3d43496d29ee91796b2e82a507
Lukas Meinhart <100217325+Drako222@users.noreply.github.com>
2022-09-11 10:53:41 +0200

    Serving file

--- myApp.js
+++ myApp.js
@@ -5,8 +5,13 @@ app.get('/', function (req, res) {
   res.send('Hello Express');
 });
 
-// console.log(app.get('/', res));
+// __dirname is Node global variable to calculate the path like this
+const absolutePath = __dirname + /views/index.html;
 
+// will server a file
+app.get('/', res.sendFile(absolutePath));
+
+// console.log(app.get('/', res));
 console.log('Hello World');
 
 // is exported to be used in servers.js
--- server.js
+++ server.js
@@ -27,6 +27,7 @@ if (!process.env.DISABLE_XORIGIN) {
   });
 }
 
+// app.listen method
 var port = process.env.PORT || 3000;
 bGround.setupBackgroundApp(app, myApp, __dirname).listen(port, function () {
   bGround.log('Node is listening on port ' + port + '...');
