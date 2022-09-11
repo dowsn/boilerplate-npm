dcae16e9f64eb4e78153684ca86cb4b5c5f9d8a0
Lukas Meinhart <100217325+dowsn@users.noreply.github.com>
2022-09-11 18:56:31 +0000

    Post method

--- myApp.js
+++ myApp.js
@@ -1,6 +1,23 @@
 let express = require('express');
+let bodyParser = require('body-parser')
 let app = express();
 require('dotenv').config();
+
+// middleware must be here mounted before all the routes that depend on it.
+
+
+//The middleware to handle URL encoded data is returned by this 
+
+// To parse the data coming from POST requests, you must use \
+// the body-parser package. 
+
+// extended is a configuration option that tells body-parser 
+// which parsing needs to be used.
+// When extended=false it uses the classic encoding querystring 
+// library. When extended=true it uses qs library for parsing.
+app.use(bodyParser.urlencoded({extended: false})); 
+// bodyParser returns middleware function with all three parameters like req, res, next, so I have to call only urlencoded method on bodyParser object and pass argument
+  
 
 // middleware function express.static(path) - interprets route handler and
 // adds some information to it; app.use is just mounting this middle ware function
@@ -63,12 +80,35 @@ app.get('/now', function(req, res, next) {
 })
 
 app.get('/:word/echo',function(req, res) {
+  console.log(req.params);
   const word = req.params.word;
   const data = {
     echo: word
   };
   res.json(data);
 });
+
+// I can chain methods like this on route
+app.route('/name')
+  .get(function(req, res) {
+  const first = req.query.first;
+  const last = req.query.last;
+  const data = {
+    name: `${first} ${last}`
+  };
+  res.json(data);
+})
+  // for the form after hitting submit I have three keys in req
+  // object and I can return them because I can parse body due
+  // to library
+  .post(function(req, res) {
+    console.log(req.body);
+    res.json({
+      name: `${req.body.first} ${req.body.last}`
+    })
+  })
+
+
 
 // is exported to be used in servers.js
 module.exports = app;
