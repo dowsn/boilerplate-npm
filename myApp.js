044dff53794b71bd839df3a7e8916ab4f563cb0c
Lukas Meinhart <100217325+Drako222@users.noreply.github.com>
2022-09-11 16:41:49 +0200

    ENV added

--- myApp.js
+++ myApp.js
@@ -9,14 +9,14 @@ let app = express();
 // __dirname is Node global variable to calculate the path like this
 
 // will server a file
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   const absolutePath = __dirname + '/views/index.html';
 
   console.log(absolutePath);
-  res.sendFile(absolutePath)
-} );
+  res.sendFile(absolutePath);
+});
 
-// middleware function express.static(path) - interprets route handler and 
+// middleware function express.static(path) - interprets route handler and
 // adds some information to it; app.use is just mounting this middle ware function
 const stylesheet = __dirname + '/public';
 // I need to specify path as the first argument because it would
@@ -26,11 +26,16 @@ app.use('/public', express.static(stylesheet));
 // console.log(app.get('/', res));
 console.log('Hello World');
 
+const messageStyle = process.env.MESSAGE_STYLE;
 
 // route handler is the function within this method - function(req,res)
-  app.get('/json', function(req,res) {
-    res.json({"message": "Hello json"});
-  });
+app.get('/json', function (req, res) {
+  if (messageStyle === 'uppercase') {
+    res.json({ message: 'HELLO JSON' });
+  } else {
+    res.json({ message: 'Hello json' });
+  }
+});
 
 // is exported to be used in servers.js
 module.exports = app;
