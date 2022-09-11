b5ebdf31aa26e8c0bb135803be7e8a2d9a744828
Lukas Meinhart <100217325+Drako222@users.noreply.github.com>
2022-09-11 10:37:05 +0200

    GET method

--- myApp.js
+++ myApp.js
@@ -1,14 +1,13 @@
 let express = require('express');
 let app = express();
 
-// app.get(function (req, res) {
-//   if (req === '/') {
-//     res.send('Hello Express');
-//   }
-// });
+app.get('/', function (req, res) {
+  res.send('Hello Express');
+});
 
 // console.log(app.get('/', res));
 
 console.log('Hello World');
 
+// is exported to be used in servers.js
 module.exports = app;
