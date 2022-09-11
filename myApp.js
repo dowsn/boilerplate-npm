6a46d12e2eb21f943f42459abe6e19d8abb2b380
Lukas Meinhart <100217325+dowsn@users.noreply.github.com>
2022-09-11 14:37:14 +0000

    Added styles

--- myApp.js
+++ myApp.js
@@ -1,18 +1,36 @@
 let express = require('express');
 let app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello Express');
-});
+// just for simply showing hello word in app as a message
+// app.get('/', function (req, res) {
+//   res.send('Hello Express');
+// });
 
 // __dirname is Node global variable to calculate the path like this
-const absolutePath = __dirname + '/views/index.html';
 
 // will server a file
-app.get('/', res.sendFile(absolutePath));
+app.get('/', function(req, res) {
+  const absolutePath = __dirname + '/views/index.html';
+
+  console.log(absolutePath);
+  res.sendFile(absolutePath)
+} );
+
+// middleware function express.static(path) - interprets route handler and 
+// adds some information to it; app.use is just mounting this middle ware function
+const stylesheet = __dirname + '/public';
+// I need to specify path as the first argument because it would
+// execute middleware for all requests
+app.use('/public', express.static(stylesheet));
 
 // console.log(app.get('/', res));
 console.log('Hello World');
+
+
+// route handler is the function within this method - function(req,res)
+  app.get('/json', function(req,res) {
+    res.json({"message": "Hello json"});
+  });
 
 // is exported to be used in servers.js
 module.exports = app;
